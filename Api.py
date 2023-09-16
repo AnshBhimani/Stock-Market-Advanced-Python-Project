ab7c55b2940fe0abd6f319cc2abb032493b30741
AnshBhimani <anshbhimani2003@gmail.com>
2023-09-16 09:49:00 +0530

    Solved the siisue of repeated entries of stock

--- Api.py
+++ Api.py
@@ -38,50 +38,51 @@ try:
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
 
-    while True:
-        current_time = datetime.now().time()
-        ist_start_time = dt_time(9, 15)  # 9.15 AM IST
-        ist_end_time = dt_time(15, 30)   # 3.30 PM IST
+    if csv_file_exists:
+        while True:
+            current_time = datetime.now().time()
+            ist_start_time = dt_time(9, 15)  # 9.15 AM IST
+            ist_end_time = dt_time(15, 30)   # 3.30 PM IST
 
-        # Check if the current time is within market hours
-        if ist_start_time <= current_time <= ist_end_time:
-            # Fetch stock information for all stocks
-            for stock_code, stock_name in b.getScripCodes().items():
-                try:
-                    stock_info = b.getQuote(stock_code)
-                    stock_data = {
-                        'Stock Code': stock_code,
-                        'Stock Name': stock_name,
-                        'Current Value': stock_info.get('currentValue', ''),
-                        'Change': stock_info.get('change', ''),
-                        'Percentage Change': stock_info.get('pChange', ''),
-                        'Day High': stock_info.get('dayHigh', ''),
-                        'Day Low': stock_info.get('dayLow', ''),
-                        'Month High/Low': stock_info.get('52weekHighLow', ''),  # Corrected key
-                        'Market Cap': stock_info.get('marketCap', ''),  # Corrected key
-                        'Industry': stock_info.get('group', '')  # Corrected key
-                    }
+            # Check if the current time is within market hours
+            if ist_start_time <= current_time <= ist_end_time:
+                # Fetch stock information for all stocks
+                for stock_code, stock_name in b.getScripCodes().items():
+                    try:
+                        stock_info = b.getQuote(stock_code)
+                        stock_data = {
+                            'Stock Code': stock_code,
+                            'Stock Name': stock_name,
+                            'Current Value': stock_info.get('currentValue', ''),
+                            'Change': stock_info.get('change', ''),
+                            'Percentage Change': stock_info.get('pChange', ''),
+                            'Day High': stock_info.get('dayHigh', ''),
+                            'Day Low': stock_info.get('dayLow', ''),
+                            'Month High/Low': stock_info.get('52weekHighLow', ''),
+                            'Market Cap': stock_info.get('marketCap', ''), 
+                            'Industry': stock_info.get('Industry', '') 
+                        }
 
-                    # Append the stock's data to the CSV file
-                    with open(csv_filename, mode='a', newline='', encoding='utf-8') as csv_file:
-                        fieldnames = ['Stock Code', 'Stock Name', 'Current Value', 'Change', 'Percentage Change', 'Day High', 'Day Low', 'Month High/Low', 'Market Cap', 'Industry']
-                        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                        writer.writerow(stock_data)
+                        # Append the stock's data to the CSV file
+                        with open(csv_filename, mode='a', newline='', encoding='utf-8') as csv_file:
+                            fieldnames = ['Stock Code', 'Stock Name', 'Current Value', 'Change', 'Percentage Change', 'Day High', 'Day Low', 'Month High/Low', 'Market Cap', 'Industry']
+                            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                            writer.writerow(stock_data)
 
-                    processed_stocks += 1
-                    logger.info(f"Processed: {processed_stocks}/{total_stocks} - Stock Code: {stock_code}")
+                        processed_stocks += 1
+                        logger.info(f"Processed: {processed_stocks}/{total_stocks} - Stock Code: {stock_code}")
 
-                except InvalidStockException:
-                    logger.warning(f"Stock {stock_code} is inactive.")
+                    except InvalidStockException:
+                        logger.warning(f"Stock {stock_code} is inactive.")
 
-            logger.info("Data updating completed.")
+                logger.info("Data updating completed.")
 
-        else:
-            logger.info("Markets are closed now!! Please come back tomorrow between 9.15 AM and 3.30 PM")
-            print("Markets are closed now!! Please come back tomorrow between 9.15 AM and 3.30 PM")
-            exit()  # Exit the program
+            else:
+                logger.info("Markets are closed now!! Please come back tomorrow between 9.15 AM and 3.30 PM")
+                print("Markets are closed now!! Please come back tomorrow between 9.15 AM and 3.30 PM")
+                exit()  # Exit the program
 
-        time.sleep(1)
+            time.sleep(1)
 # The code snippet fetches stock information from the Bombay Stock Exchange (BSE) and writes it to a CSV file. It checks if the stock market is open and updates the data accordingly. The script logs various events and handles exceptions.
 
 except KeyboardInterrupt:
