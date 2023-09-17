89eae3a6d17ff23ad048222245f6fee29db6ddae
AnshBhimani <anshbhimani2003@gmail.com>
2023-09-17 11:38:05 +0530

    Added new updated Files
    Updated on frontend to show markets are closed

--- Api.py
+++ Api.py
@@ -1,10 +1,11 @@
 import logging
 from bsedata.bse import BSE
+from bselib.bse import BSE as BSE2
 from bsedata.exceptions import InvalidStockException
 import csv
 import os
 import time
-from datetime import datetime, time as dt_time
+from datetime import datetime, time as dt_time, timedelta
 
 # Configure logging settings
 logging.basicConfig(
@@ -13,13 +14,14 @@ logging.basicConfig(
     filename='app.log',
     filemode='a'
 )
-logger = logging.getLogger(__name__)  # Corrected the logger name
+logger = logging.getLogger(__name__)
 
 print("Script Started")
 logger.info("Script started.")
 
 try:
     b = BSE(update_codes=True)
+    b2 = BSE2()
 
     # Define the CSV file name
     csv_filename = "bse_stocks.csv"
@@ -28,65 +30,158 @@ try:
     total_stocks = len(b.getScripCodes())
     processed_stocks = 0
 
-    # Check if the CSV file exists
-    csv_file_exists = os.path.exists(csv_filename)
-
-    if not csv_file_exists:
-        logger.info(f"CSV file '{csv_filename}' does not exist. Creating a new one.")
+    # Phase 1: Initial listing of all stocks in the CSV
+    if not os.path.exists(csv_filename):
         fieldnames = ['Stock Code', 'Stock Name', 'Current Value', 'Change', 'Percentage Change', 'Day High', 'Day Low', 'Month High/Low', 'Market Cap', 'Industry']
         with open(csv_filename, mode='w', newline='', encoding='utf-8') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
 
-    if csv_file_exists:
-        while True:
-            current_time = datetime.now().time()
-            ist_start_time = dt_time(9, 15)  # 9.15 AM IST
-            ist_end_time = dt_time(15, 30)   # 3.30 PM IST
+            for stock_code, stock_name in b.getScripCodes().items():
+                writer.writerow({
+                    'Stock Code': stock_code,
+                    'Stock Name': stock_name,
+                    'Current Value': '',
+                    'Change': '',
+                    'Percentage Change': '',
+                    'Day High': '',
+                    'Day Low': '',
+                    'Month High/Low': '',
+                    'Market Cap': '',
+                    'Industry': ''
+                })
 
-            # Check if the current time is within market hours
-            if ist_start_time <= current_time <= ist_end_time:
-                # Fetch stock information for all stocks
-                for stock_code, stock_name in b.getScripCodes().items():
-                    try:
-                        stock_info = b.getQuote(stock_code)
-                        stock_data = {
-                            'Stock Code': stock_code,
-                            'Stock Name': stock_name,
-                            'Current Value': stock_info.get('currentValue', ''),
-                            'Change': stock_info.get('change', ''),
-                            'Percentage Change': stock_info.get('pChange', ''),
-                            'Day High': stock_info.get('dayHigh', ''),
-                            'Day Low': stock_info.get('dayLow', ''),
-                            'Month High/Low': stock_info.get('52weekHighLow', ''),
-                            'Market Cap': stock_info.get('marketCap', ''), 
-                            'Industry': stock_info.get('Industry', '') 
-                        }
+                processed_stocks += 1
+                logger.info(f"Processed: {processed_stocks}/{total_stocks} - Stock Code: {stock_code}")
 
-                        # Append the stock's data to the CSV file
-                        with open(csv_filename, mode='a', newline='', encoding='utf-8') as csv_file:
-                            fieldnames = ['Stock Code', 'Stock Name', 'Current Value', 'Change', 'Percentage Change', 'Day High', 'Day Low', 'Month High/Low', 'Market Cap', 'Industry']
-                            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                            writer.writerow(stock_data)
+        logger.info("All stocks listed in the CSV.")
+    else:
+        logger.warning(f"CSV file '{csv_filename}' already exists.")
 
-                        processed_stocks += 1
-                        logger.info(f"Processed: {processed_stocks}/{total_stocks} - Stock Code: {stock_code}")
+    # Controlled loop between 9:15 AM and 3:30 PM IST on weekdays
+    while True:
+        days_on = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+        current_day = datetime.today().strftime("%A")
+        current_time = datetime.now().time()
+        ist_start_time = dt_time(9, 15)  # 9.15 AM IST
+        ist_end_time = dt_time(15, 30)   # 3.30 PM IST
 
-                    except InvalidStockException:
-                        logger.warning(f"Stock {stock_code} is inactive.")
+        if (
+            ist_start_time <= current_time <= ist_end_time
+            and current_day in days_on
+        ):
+            with open(csv_filename, mode='r', newline='', encoding='utf-8') as csv_file:
+                rows = list(csv.DictReader(csv_file))
 
-                logger.info("Data updating completed.")
+            updated_rows = []
 
+            for index, row in enumerate(rows):
+                try:
+                    stock_info = b.getQuote(row['Stock Code'])
+                    logger.info(f"Available keys in stock_info: {stock_info.keys()}")
+
+                    updated_row = row.copy()
+                    updated_row['Current Value'] = stock_info.get('currentValue', '')
+                    updated_row['Change'] = stock_info.get('change', '')
+                    updated_row['Percentage Change'] = stock_info.get('pChange', '')
+                    updated_row['Day High'] = stock_info.get('dayHigh', '')  # Corrected key
+                    updated_row['Day Low'] = stock_info.get('dayLow', '')  # Corrected key
+                    updated_row['Market Cap'] = stock_info.get('marketCapFull', '')  # Corrected key
+                    updated_row['Month High/Low'] = stock_info.get('monthHighLow', '')  # Corrected key
+
+                    updated_rows.append(updated_row)
+
+                    logger.info(f"Updated: {index + 1}/{total_stocks} - Stock Code: {row['Stock Code']}")
+                except InvalidStockException:
+                    logger.warning(f"Stock {row['Stock Code']} is inactive.")
+
+            # Write the updated rows back to the CSV
+            with open(csv_filename, mode='w', newline='', encoding='utf-8') as csv_file:
+                fieldnames = ['Stock Code', 'Stock Name', 'Current Value', 'Change', 'Percentage Change', 'Day High', 'Day Low', 'Month High/Low', 'Market Cap', 'Industry']
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerows(updated_rows)
+
+            # New functionality: Analysis Reports for all stocks
+            csv_dir = "analysis_reports"
+            os.makedirs(csv_dir, exist_ok=True)
+
+            for row in rows:
+                stock_code_to_analyze = row['Stock Code']
+
+                # Cashflow
+                his_data = b2.historical_stats(stock_code_to_analyze, stats="cashflow")
+                his_data_path = os.path.join(csv_dir, f"{stock_code_to_analyze}_cashflow.csv")
+                with open(his_data_path, "w") as file:
+                    for key, value in his_data.items():
+                        file.write(f"{key},{value}\n")
+
+                # YOY Results
+                his_data2 = b2.historical_stats(stock_code_to_analyze, stats="yoy_results")
+                his_data2_path = os.path.join(csv_dir, f"{stock_code_to_analyze}_yoy_results.csv")
+                with open(his_data2_path, "w") as file:
+                    for key, value in his_data2.items():
+                        file.write(f"{key},{value}\n")
+
+                # Performance Analysis
+                performance_analysis = b2.analysis(stock_code_to_analyze)
+                performance_analysis_path = os.path.join(csv_dir, f"{stock_code_to_analyze}_performance_analysis.csv")
+                with open(performance_analysis_path, "w") as file:
+                    for key, value in performance_analysis.items():
+                        if isinstance(value, str):
+                            value = value.replace(",", ";")
+                        file.write(f"{key},{value}\n")
+
+                # Holding Info
+                holding_info = b2.holdings(stock_code_to_analyze)
+                holding_info_path = os.path.join(csv_dir, f"{stock_code_to_analyze}_holding_info.csv")
+                with open(holding_info_path, "w") as file:
+                    for key, value in holding_info.items():
+                        file.write(f"{key},{value}\n")
+
+                # Annual Reports
+                annual_reports = b2.annual_reports(stock_code_to_analyze)
+                annual_reports_path = os.path.join(csv_dir, f"{stock_code_to_analyze}_annual_reports.csv")
+                with open(annual_reports_path, "w") as file:
+                    for key, value in annual_reports.items():
+                        file.write(f"{key},{value}\n")
+
+                # Credit Reports
+                credit_reports = b2.credit_reports(stock_code_to_analyze)
+                credit_reports_path = os.path.join(csv_dir, f"{stock_code_to_analyze}_credit_reports.csv")
+                with open(credit_reports_path, "w") as file:
+                    for key, value in credit_reports.items():
+                        file.write(f"{key},{value}\n")
+
+            logger.info("Analysis reports saved for all stocks in the directory: %s", csv_dir)
+
+            current_datetime = datetime.now()
+            end_datetime = datetime.combine(current_datetime.date(), ist_end_time)
+            if current_datetime > end_datetime:
+                next_day = current_datetime + timedelta(days=1)
+                next_start_datetime = datetime.combine(next_day.date(), ist_start_time)
+                sleep_duration = (next_start_datetime - current_datetime).total_seconds()
             else:
-                logger.info("Markets are closed now!! Please come back tomorrow between 9.15 AM and 3.30 PM")
-                print("Markets are closed now!! Please come back tomorrow between 9.15 AM and 3.30 PM")
-                exit()  # Exit the program
+                sleep_duration = (end_datetime - current_datetime).total_seconds()
 
-            time.sleep(1)
-# The code snippet fetches stock information from the Bombay Stock Exchange (BSE) and writes it to a CSV file. It checks if the stock market is open and updates the data accordingly. The script logs various events and handles exceptions.
+            logger.info("Data updating completed.")
+            logger.info("Sleeping until the next update time.")
+            time.sleep(sleep_duration)
+
+        else:
+            logger.info("Markets are closed now!! Please come back between 9.15 AM and 3.30 PM on Weekdays")
+            print("Markets are closed now!! Please come back between 9.15 AM and 3.30 PM on Weekdays")
+            current_datetime = datetime.now()
+            next_weekday = current_datetime + timedelta(days=(1 - current_datetime.weekday() + 7) % 7)
+            next_open_time = datetime.combine(next_weekday.date(), ist_start_time)
+            sleep_duration = (next_open_time - current_datetime).total_seconds()
+
+            logger.info(f"Sleeping until the market opens on the next weekday at {ist_start_time}.")
+            time.sleep(sleep_duration)
+            exit()  # Exit the program
 
 except KeyboardInterrupt:
-    logger.critical("Script interrupted by the user.")
+    logger.critical("Script interrupted by user.")
 except Exception as e:
     logger.error("An error occurred:", exc_info=True)
 finally:
--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import os
 import time
 import colorlog
 from flask import Flask, render_template, request, jsonify
+from threading import Thread
 import logging
 import pandas as pd
+from datetime import datetime, time as dt_time, timedelta
 from bsedata.exceptions import InvalidStockException
 from bsedata.bse import BSE
-from bselib.bse import BSE as BSE2
 import csv
 import webbrowser
 import subprocess
-from threading import Thread
 
 # Check and install the dependencies 
 # List of dependencies to check
@@ -59,6 +59,7 @@ app = Flask(__name__)
 class StockUpdater:
     def __init__(self):
         self.running = False
+        self.markets_open = False
         self.thread = None
 
     def start(self):
@@ -71,128 +72,78 @@ class StockUpdater:
         self.running = False
         if self.thread:
             self.thread.join()
-        
+
     def update_loop(self):
         try:
             b = BSE(update_codes=True)
 
             while self.running:
-                csv_filename = 'bse_stocks.csv'
-                if not os.path.exists(csv_filename):
-                    with open(csv_filename, mode='w', newline='', encoding='utf-8') as csv_file:
-                        fieldnames = ['Stock Code', 'Stock Name', 'Current Value', 'Change', 'Percentage Change', 'Market Cap', 'Industry', 'Day High', 'Day Low', 'Month High/Low']
+                days_on = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+                current_day = datetime.today().strftime("%A")
+                current_time = datetime.now().time()
+                ist_start_time = dt_time(9, 15)  # 9.15 AM IST
+                ist_end_time = dt_time(15, 30)   # 3.30 PM IST
+
+                if (
+                    ist_start_time <= current_time <= ist_end_time
+                    and current_day in days_on):
+                    
+                    self.markets_open = True
+                    csv_filename = 'bse_stocks.csv'
+                    if not os.path.exists(csv_filename):
+                        with open(csv_filename, mode='w', newline='', encoding='utf-8') as csv_file:
+                            fieldnames = ['Stock Code', 'Stock Name', 'Current Value', 'Change', 'Percentage Change', 'Market Cap', 'Industry', 'Day High', 'Day Low', 'Month High/Low']
+                            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                            writer.writeheader()
+
+                    with open('bse_stocks.csv', mode='r', newline='', encoding='utf-8') as csv_file:
+                        rows = list(csv.DictReader(csv_file))
+
+                    for index, row in enumerate(rows):
+                        try:
+                            stock_info = b.getQuote(row['Stock Code'])
+                            logger.info(f"Available keys in stock_info: {stock_info.keys()}")
+
+                            rows[index]['Current Value'] = stock_info.get('currentValue', '')
+                            rows[index]['Change'] = stock_info.get('change', '')
+                            rows[index]['Percentage Change'] = stock_info.get('pChange', '')
+                            rows[index]['Day High'] = stock_info.get('dayHigh', '')
+                            rows[index]['Day Low'] = stock_info.get('dayLow', '')
+                            rows[index]['Market Cap'] = stock_info.get('marketCapFull', '')
+                            rows[index]['Month High/Low'] = stock_info.get('monthHighLow', '')
+
+                            logger.info(f"Updated: {index + 1}/{len(rows)} - Stock Code: {row['Stock Code']}")
+                        except InvalidStockException:
+                            logger.warning(f"Stock {row['Stock Code']} is inactive.")
+                        except Exception as e:
+                            logger.error(f"An error occurred while updating stock info for {row['Stock Code']}: {e}")
+
+                    # Write the updated rows back to the CSV
+                    with open('bse_stocks.csv', mode='w', newline='', encoding='utf-8') as csv_file:
+                        fieldnames = ['Stock Code', 'Stock Name', 'Current Value', 'Change', 'Percentage Change', 'Day High', 'Day Low', 'Month High/Low', 'Market Cap', 'Industry']
                         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
                         writer.writeheader()
+                        writer.writerows(rows)
 
-                with open('bse_stocks.csv', mode='r', newline='', encoding='utf-8') as csv_file:
-                    rows = list(csv.DictReader(csv_file))
-
-                for index, row in enumerate(rows):
-                    try:
-                        stock_info = b.getQuote(row['Stock Code'])
-                        logger.info(f"Available keys in stock_info: {stock_info.keys()}")
-
-                        rows[index]['Current Value'] = stock_info.get('currentValue', '')
-                        rows[index]['Change'] = stock_info.get('change', '')
-                        rows[index]['Percentage Change'] = stock_info.get('pChange', '')
-                        rows[index]['Day High'] = stock_info.get('dayHigh', '')
-                        rows[index]['Day Low'] = stock_info.get('dayLow', '')
-                        rows[index]['Market Cap'] = stock_info.get('marketCapFull', '')
-                        rows[index]['Month High/Low'] = stock_info.get('monthHighLow', '')
-
-                        logger.info(f"Updated: {index + 1}/{len(rows)} - Stock Code: {row['Stock Code']}")
-                    except InvalidStockException:
-                        logger.warning(f"Stock {row['Stock Code']} is inactive.")
-                    except Exception as e:
-                        logger.error(f"An error occurred while updating stock info for {row['Stock Code']}: {e}")
-
-                # Write the updated rows back to the CSV
-                with open('bse_stocks.csv', mode='w', newline='', encoding='utf-8') as csv_file:
-                    fieldnames = ['Stock Code', 'Stock Name', 'Current Value', 'Change', 'Percentage Change', 'Day High', 'Day Low', 'Month High/Low', 'Market Cap', 'Industry']
-                    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                    writer.writeheader()
-                    writer.writerows(rows)
-
-                logger.info("Data updating completed.")
-                time.sleep(10)
-
+                    logger.info("Data updating completed.")
+                    time.sleep(10)
+                
+                else:
+                    self.markets_open = False
+                    logger.info("Markets are closed now!! Please come back between 9.15 AM and 3.30 PM on Weekdays")
+                    time.sleep(600)  # Sleep for 10 minutes and check again
+                    
         except Exception as e:
             logger.error("An error occurred:", exc_info=True)
         finally:
             self.running = False
 
-    def fetch_and_analyze_data(self, stock_code):
-        b2 = BSE2()
-
-        # Initialize data dictionaries for different parameters
-        cashflow_data = {}
-        yoy_results_data = {}
-        performance_analysis_data = {}
-        annual_reports_data = {}
-
-        try:
-            # Fetch data for each parameter
-            cashflow_data = b2.historical_stats(stock_code, stats="cashflow")
-            yoy_results_data = b2.historical_stats(stock_code, stats="yoy_results")
-            performance_analysis_data = b2.analysis(stock_code)
-            annual_reports_data = b2.annual_reports(stock_code)
-
-            # Calculate average, minimum, and maximum values for each parameter
-            cashflow_average = sum(cashflow_data.values()) / len(cashflow_data)
-            cashflow_min = min(cashflow_data.values())
-            cashflow_max = max(cashflow_data.values())
-
-            yoy_results_average = sum(yoy_results_data.values()) / len(yoy_results_data)
-            yoy_results_min = min(yoy_results_data.values())
-            yoy_results_max = max(yoy_results_data.values())
-
-            # You can similarly calculate average, min, and max for performance_analysis and annual_reports
-            performance_analysis_average = sum(performance_analysis_data.values()) / len(performance_analysis_data)
-            performance_analysis_min = min(performance_analysis_data.values())
-            performance_analysis_max = max(performance_analysis_data.values())
-
-            annual_reports_average = sum(annual_reports_data.values()) / len(annual_reports_data)
-            annual_reports_min = min(annual_reports_data.values())
-            annual_reports_max = max(annual_reports_data.values())
-
-            performance_metrics = {
-                'Cashflow': {
-                    'Data': cashflow_data,
-                    'Average': cashflow_average,
-                    'Min': cashflow_min,
-                    'Max': cashflow_max
-                },
-                'YOY Results': {
-                    'Data': yoy_results_data,
-                    'Average': yoy_results_average,
-                    'Min': yoy_results_min,
-                    'Max': yoy_results_max
-                },
-                'Performance Analysis': {
-                    'Data': performance_analysis_data,
-                    'Average': performance_analysis_average,
-                    'Min': performance_analysis_min,
-                    'Max': performance_analysis_max
-                },
-                'Annual Reports': {
-                    'Data': annual_reports_data,
-                    'Average': annual_reports_average,
-                    'Min': annual_reports_min,
-                    'Max': annual_reports_max
-                }
-            }
-
-            return performance_metrics
-
-        except InvalidStockException:
-            logger.error("Invalid Stock for analysis")
-            return "Invalid Stock"
-
 updater = StockUpdater()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    updater.start()
+    return render_template('index.html', markets_open = updater.markets_open)
 
 @app.route('/analyze')
 def analyse_dashboard():
@@ -236,9 +187,6 @@ if __name__ == "__main__":
                 writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
                 writer.writeheader()
 
-        # Start the Api.py script in a separate process
-        api_process = subprocess.Popen(["python", "Api.py"])
-
         app.run(debug=True, port=8000)
 
         # Open the browser automatically
@@ -247,6 +195,6 @@ if __name__ == "__main__":
         print("App running on url : " + url)
 
     except KeyboardInterrupt:
-        logger.critical("Script interrupted by the user.")
+        logger.critical("Script interrupted by user.")
     except Exception as e:
         logger.error("An error occurred:", exc_info=True)
